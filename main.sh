45f00593df5cdf9f7eef8984bd49115f5dcadede
oliynykmax <olejnikmax@gmail.com>
2026-02-03 13:26:26 +0200

    audo deploy now

---
+++ main.sh
@@ -0,0 +1,7 @@
+aws configure --profile awsbootstrap
+AWS Access Key ID [None]: EXAMPLE_KEY
+AWS Secret Access Key [None]: SECRETKEYEXAMPLE
+Default region name [None]: eu-north-1
+Default output format [None]: json
+
+aws ec2 describe-instances --profile awsbootstrap
